c2747daa75328c467556b1a2503fc4607e7589ff
Ashiqul Hoque <ronohoque@gmail.com>
2020-10-11 19:02:17 +0600

    REMOVING UNNECESSARY IMPORTS

--- src/main/java/com/bstation/iamin/Controller/HomeController.java
+++ src/main/java/com/bstation/iamin/Controller/HomeController.java
@@ -1,17 +1,10 @@
 package com.bstation.iamin.Controller;
 
 import com.bstation.iamin.Entity.Post;
-import com.bstation.iamin.Entity.User;
 import com.bstation.iamin.Repository.PostRepository;
-import com.bstation.iamin.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
--- src/main/java/com/bstation/iamin/Controller/LoginController.java
+++ src/main/java/com/bstation/iamin/Controller/LoginController.java
@@ -1,12 +1,10 @@
 package com.bstation.iamin.Controller;
 
-import com.bstation.iamin.Entity.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 @RequestMapping
